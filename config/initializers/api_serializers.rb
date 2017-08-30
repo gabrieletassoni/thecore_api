ed9ff32a7365ba6e5d7c700755cd58d9d5b214af
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2017-08-30 14:21:46 +0200

    active model serializer configuration

--- config/initializers/api_serializers.rb
+++ config/initializers/api_serializers.rb
@@ -6,4 +6,5 @@ ActiveModel::Serializer.setup do |config|
   # Answer with id not the entire object, for has_many and belongs_to associations
   # This limits the throughput
   config.embed = :ids
+  config.adapter = :json_api
 end
