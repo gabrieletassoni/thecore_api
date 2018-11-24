82e06f3396272ee45bfbf5f0d695f56d80fae894
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-11-24 18:22:01 +0100

    cors only all

--- config/initializers/cors_api_thecore.rb
+++ config/initializers/cors_api_thecore.rb
@@ -1,15 +1,16 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins { |source, env| 
-      # No settings => '*'
-      allowed_origins = Settings.ns('api').allowed_origins
-      return true if allowed_origins.blank?
-      allowed_origins.split(",").each do |domain|
-        return true if source.end_with?(domain.strip)
-      end
-      return false 
-    }
+    # origins { |source, env| 
+    #   # No settings => '*'
+    #   allowed_origins = Settings.ns('api').allowed_origins
+    #   return true if allowed_origins.blank?
+    #   allowed_origins.split(",").each do |domain|
+    #     return true if source.end_with?(domain.strip)
+    #   end
+    #   return false 
+    # }
+    origins '*'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
