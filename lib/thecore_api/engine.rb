370fc0bf2d9f9f072ca4d6db7ec4687a230d45aa
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2019-04-30 16:26:58 +0200

    migration initializer for engine cleanup

--- lib/thecore_api/engine.rb
+++ lib/thecore_api/engine.rb
@@ -2,7 +2,7 @@ module ThecoreApi
   class Engine < ::Rails::Engine
     # appending migrations to the main app's ones
     initializer :append_migrations do |app|
-      unless app.root.to_s == root.to_s
+      unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
         end
